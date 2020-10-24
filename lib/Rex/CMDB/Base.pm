9a048b1d01c1e5b62f1ccb93ded00b7384b9696c
Ferenc Erki <erkiferenc@gmail.com>
2020-10-24 23:24:07 +0200

    Make CMDB file list testable

--- lib/Rex/CMDB/Base.pm
+++ lib/Rex/CMDB/Base.pm
@@ -30,4 +30,31 @@ sub _parse_path {
   return parse_path($path);
 }
 
+sub _get_cmdb_files {
+  my ( $self, $item, $server ) = @_;
+
+  $server //= Rex::get_current_connection()->{conn}->server->to_s;
+
+  my @files;
+
+  if ( !ref $self->{path} ) {
+    my $env       = Rex::Commands::environment();
+    my $yaml_path = $self->{path};
+    @files = (
+      "$yaml_path/$env/$server.yml", "$yaml_path/$env/default.yml",
+      "$yaml_path/$server.yml",      "$yaml_path/default.yml"
+    );
+  }
+  elsif ( ref $self->{path} eq "CODE" ) {
+    @files = $self->{path}->( $self, $item, $server );
+  }
+  elsif ( ref $self->{path} eq "ARRAY" ) {
+    @files = @{ $self->{path} };
+  }
+
+  @files = map { $self->_parse_path($_) } @files;
+
+  return @files;
+}
+
 1;
